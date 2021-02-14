1208f7ba11b018f94f1b3c15440e4727868677bd
achaves100 <adan.chaves@outlook.com>
2021-02-14 03:39:04 +0000

    Adan - Excuse Javascript Excersise

---
+++ index.js
@@ -0,0 +1,12 @@
+function generator () {
+    var who = ["Juan", "Maria", "Pablo", "Roberto"]; 
+    var action = ["duerme", "come", "escribe", "habla"];
+    var when = ["hoy", "despues", "durante clases", "anteayer"];
+ 
+    var msg =
+        who[Math.floor(Math.random() * who.length)] + " " +
+        action[Math.floor(Math.random() * action.length)] + " " +
+        when[Math.floor(Math.random() * when.length)];
+    document.querySelector("#excuse").innerHTML=msg;
+    }
+    console.log(generator());
